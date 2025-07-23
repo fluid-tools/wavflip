004fc038d1893500fe8b2fbad8c5b6d670aea1ae
Arth Tyagi <arthtyagi7@icloud.com>
2025-07-24 01:13:32 +0530

    up

--- components/library-content.tsx
+++ components/library-content.tsx
@@ -15,7 +15,7 @@ import {
   HardDrive,
   MoreHorizontal
 } from 'lucide-react'
-import { SoundDetailDialog } from '@/components/sound-detail-dialog'
+import { SoundDetailDialog } from '@/app/(protected)/components/sound-detail-dialog'
 import { 
   getLibraryTracks, 
   getLibraryStats,
